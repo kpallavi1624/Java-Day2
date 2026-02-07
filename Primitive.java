69a0c105305b237085e920fc87fc9706b53f3ea7
kpallavi1624 <kchandana832867@gmail.com>
2026-02-07 10:56:09 +0530

    Add files via upload

---
+++ Primitive.java
@@ -0,0 +1,15 @@
+public class Primitive {
+    public static void main(String[] args){
+        String name="zakiya";
+        int age=20;
+        char grade='a';
+        int marks=80;
+        if(marks<45){
+            System.out.println("fail");
+        }
+        else{
+            System.out.println("pass");
+        }
+    }
+    
+}
